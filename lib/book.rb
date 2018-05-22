fb53ef56540726435c360958725d5a14215bda5c
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-22 21:23:12 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,37 +1,10 @@
 class Book
 
   attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   def initialize(title)
-@title = title
-  end
-
-  attr_getter :title
-
-
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-  @genre = genre
-  end
-
-  def genre
-    @genre
+  @title = title
   end
 
   def turn_page
